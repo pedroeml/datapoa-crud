f9ee36c0bb1b604cfd7428fb2d3c0bd83a83473f
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-03-04 19:53:04 -0300

    Updating Itinerario Service

--- angular-datapoa-crud/src/app/app.module.ts
+++ angular-datapoa-crud/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,6 +16,7 @@ import { LayoutModule } from '@angular/cdk/layout';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
--- angular-datapoa-crud/src/app/itinerario-unidade/itinerario-unidade.service.ts
+++ angular-datapoa-crud/src/app/itinerario-unidade/itinerario-unidade.service.ts
@@ -1,27 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { ItinerarioUnidadeModule } from './itinerario-unidade.module';
 import { ItinerarioUnidade } from './itinerario-unidade';
+import { Coordenadas } from './coordenadas';
 
 @Injectable({
   providedIn: ItinerarioUnidadeModule
 })
 export class ItinerarioUnidadeService {
-  baseUrl = 'http://www.poatransporte.com.br/php/facades/process.php';
+  private baseUrl = 'http://www.poatransporte.com.br/php/facades/process.php';
 
   constructor(private http: HttpClient) { }
 
   getItinerario(unidadeId: string): Observable<ItinerarioUnidade> {
-    return this.http.get<any[]>(`${this.baseUrl}?a=il&p=${unidadeId}`).pipe(
-      tap(_ => console.log(`Fetched Itinerario UT ${unidadeId}`)),
-      catchError(this.handleError<any>(`getItinerario unidadeId=${unidadeId}`))
-    );
+    return this.http.get<ItinerarioUnidade>(`${this.baseUrl}?a=il&p=${unidadeId}`).pipe(
+      map(res => {
+        const coords = [];
 
-    // TODO: How to wrap the response into ItinerarioUnidade?
-    // https://angular.io/tutorial/toh-pt6#final-code-review
+        Object.keys(res).filter(key => {
+          return !isNaN(parseInt(key));
+        }).forEach(key => {
+          const coord = new Coordenadas(res[key]['lat'], res[key]['lng']);
+          coords.push(coord);
+        });
+
+        return new ItinerarioUnidade(res['idlinha'], res['nome'], res['codigo'], coords);
+      }),
+      tap(el => console.log(el),
+          err => console.error(`Error on fetching Itinerario UT ${unidadeId}`),
+          () => console.log(`Fetched Itinerario UT ${unidadeId}`)
+      ),
+      catchError(this.handleError<ItinerarioUnidade>(`getItinerario unidadeId=${unidadeId}`))
+    );
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
