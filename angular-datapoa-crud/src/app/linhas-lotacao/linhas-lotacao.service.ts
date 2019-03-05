68e3f5c718d2d0fcb665b44ad25ac6a4449094a0
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-03-05 20:43:15 -0300

    Linhas Lotação Service

---
+++ angular-datapoa-crud/src/app/linhas-lotacao/linhas-lotacao.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { LinhasLotacaoModule } from './linhas-lotacao.module';
+import { Observable, of } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
+import { Lotacao } from './lotacao';
+
+@Injectable({
+  providedIn: LinhasLotacaoModule
+})
+export class LinhasLotacaoService {
+  private baseUrl = 'http://www.poatransporte.com.br/php/facades/process.php';
+
+  constructor(private http: HttpClient) { }
+
+  getLinhasLotacao(): Observable<Lotacao[]> {
+    return this.http.get<Lotacao[]>(`${this.baseUrl}?a=nc&p=%&t=l`).pipe(
+      map(res => res.map(lotacao => new Lotacao(lotacao['id'], lotacao['nome'], lotacao['codigo']))),
+      tap(el => console.log(el),
+          err => console.error('Error on fetching Linhas de Lotação'),
+          () => console.log('Fetched Linhas de Lotação')
+      ),
+      catchError(this.handleError<Lotacao[]>(`getLinhasLotacao`))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);       // Log the error
+      return of(result as T);     // Return empty result.
+    };
+  }
+}
