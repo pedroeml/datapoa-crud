e68d90864f2d1ec247a1207b8cb9189877e956fd
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-03-05 15:56:28 -0300

    Base implementation of Linhas Ônibus List component

--- angular-datapoa-crud/src/app/linhas-onibus/linhas-onibus-list/linhas-onibus-list.component.ts
+++ angular-datapoa-crud/src/app/linhas-onibus/linhas-onibus-list/linhas-onibus-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+
+import { Onibus } from './../onibus';
+import { LinhasOnibusService } from '../linhas-onibus.service';
 
 @Component({
   selector: 'app-linhas-onibus-list',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./linhas-onibus-list.component.css']
 })
 export class LinhasOnibusListComponent implements OnInit {
+  private form: FormGroup;
+  private isLoading: boolean;
+  private onibusList: Onibus[];
+  private displayedColumns: string[];
+  private dataSource: MatTableDataSource<Onibus>;
+  private tableSizeOptions = [5, 10, 20];
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private linhasOnibusService: LinhasOnibusService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.isLoading = false;
+    this.onibusList = undefined;
+    this.dataSource = undefined;
+    this.displayedColumns = ['id', 'nome', 'codigo'];
+    this.isLoading = true
+    this.getLinhasOnibus().subscribe(onibusList => {
+      this.onibusList = onibusList;
+      if (onibusList) {
+        this.dataSource = new MatTableDataSource(this.onibusList);
+        this.dataSource.paginator = this.paginator;
+      }
+      this.isLoading = false;
+    });
+  }
+
+  private getLinhasOnibus(): Observable<Onibus[]> {
+    return this.linhasOnibusService.getLinhasOnibus();
   }
 
 }
