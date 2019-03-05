a5de3f9f265b961731000b0026126df6161fafd3
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-03-05 20:44:46 -0300

    Linhas Lotação List component

--- angular-datapoa-crud/src/app/linhas-lotacao/linhas-lotacao-list/linhas-lotacao-list.component.ts
+++ angular-datapoa-crud/src/app/linhas-lotacao/linhas-lotacao-list/linhas-lotacao-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Lotacao } from './../lotacao';
+import { LinhasLotacaoService } from '../linhas-lotacao.service';
 
 @Component({
   selector: 'app-linhas-lotacao-list',
@@ -6,10 +13,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./linhas-lotacao-list.component.css']
 })
 export class LinhasLotacaoListComponent implements OnInit {
+  private form: FormGroup;
+  private isLoading: boolean;
+  private lotacaoList: Lotacao[];
+  private displayedColumns: string[];
+  private dataSource: MatTableDataSource<Lotacao>;
+  private tableSizeOptions = [5, 10, 20];
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private linhasLotacaoService: LinhasLotacaoService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.isLoading = false;
+    this.lotacaoList = undefined;
+    this.dataSource = undefined;
+    this.displayedColumns = ['id', 'nome', 'codigo'];
+    this.form = this.formBuilder.group({
+      filterCtrl: ['', []]
+    });
+    this.form.get('filterCtrl').valueChanges.pipe(
+      tap(_ => (this.isLoading = true)),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(search => this.applyFilter(search)),
+      tap(_ => (this.isLoading = false))).subscribe();
+    this.isLoading = true;
+    this.getLinhasLotacao().subscribe(lotacaoList => {
+      this.lotacaoList = lotacaoList;
+      if (lotacaoList) {
+        this.dataSource = new MatTableDataSource(this.lotacaoList);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      }
+      this.isLoading = false;
+    });
+  }
+
+  private getLinhasLotacao(): Observable<Lotacao[]> {
+    return this.linhasLotacaoService.getLinhasLotacao();
+  }
+
+  applyFilter(filterValue: string): Lotacao[] {
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+    return this.dataSource.filteredData;
   }
 
 }
--- angular-datapoa-crud/src/app/linhas-lotacao/linhas-lotacao.module.ts
+++ angular-datapoa-crud/src/app/linhas-lotacao/linhas-lotacao.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatTableModule, MatIconModule, MatPaginatorModule, MatSortModule, MatInputModule } from '@angular/material';
 import { LinhasLotacaoRoutingModule } from './linhas-lotacao-routing.module';
 import { LinhasLotacaoListComponent } from './linhas-lotacao-list/linhas-lotacao-list.component';
+import { LinhasLotacaoService } from './linhas-lotacao.service';
 
 @NgModule({
   declarations: [LinhasLotacaoListComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
     MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatSortModule,
     LinhasLotacaoRoutingModule
+  ],
+  providers: [
+    LinhasLotacaoService
   ]
 })
 export class LinhasLotacaoModule { }
