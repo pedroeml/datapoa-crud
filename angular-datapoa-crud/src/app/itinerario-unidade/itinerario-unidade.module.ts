3c6cd519e6c9f0e7dbca1b65dda0effa8279ab06
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-03-05 23:08:15 -0300

    Adding MatTooltip to Itinerário component

--- angular-datapoa-crud/src/app/itinerario-unidade/itinerario-unidade.module.ts
+++ angular-datapoa-crud/src/app/itinerario-unidade/itinerario-unidade.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule, MatFormFieldModule, MatProgressSpinnerModule, MatCardModule, MatTableModule, MatIconModule, MatPaginatorModule, MatSnackBarModule } from '@angular/material';
+import { MatInputModule, MatFormFieldModule, MatProgressSpinnerModule, MatCardModule, MatTableModule, MatIconModule, MatPaginatorModule, MatSnackBarModule, MatTooltipModule } from '@angular/material';
 import { ItinerarioUnidadeRoutingModule } from './itinerario-unidade-routing.module';
 import { ItinerarioUnidadeDetailsComponent } from './itinerario-unidade-details/itinerario-unidade-details.component';
 import { ItinerarioUnidadeService } from './itinerario-unidade.service';
@@ -20,6 +20,7 @@ import { ItinerarioUnidadeService } from './itinerario-unidade.service';
     MatIconModule,
     MatPaginatorModule,
     MatSnackBarModule,
+    MatTooltipModule,
     ItinerarioUnidadeRoutingModule
   ],
   providers: [
