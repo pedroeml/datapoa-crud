444c95cc41a6e86ba6e4b37b1179512fb2f1e094
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2019-03-05 23:08:52 -0300

    Adding content to Welcome component

--- angular-datapoa-crud/src/app/home/home.module.ts
+++ angular-datapoa-crud/src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material';
+import { MatCardModule, MatGridListModule, MatTooltipModule } from '@angular/material';
 import { HomeRoutingModule } from './home-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -9,6 +9,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
   imports: [
     CommonModule,
     MatCardModule,
+    MatGridListModule,
+    MatTooltipModule,
     HomeRoutingModule
   ]
 })
--- angular-datapoa-crud/src/app/home/welcome/welcome.component.ts
+++ angular-datapoa-crud/src/app/home/welcome/welcome.component.ts
@@ -1,11 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  text: string;
+  cols: number;
+  rows: number;
+  color: string;
+  imageUrl: string;
+}
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  private tiles: Tile[] = [{
+    text: 'Esta aplicação foi desenvolvida utilizando Angular CLI 7.',
+    cols: 2,
+    rows: 2,
+    color: '#1976d2',
+    imageUrl: 'assets/images/angular_logo.png'
+  }, {
+    text: 'A interface desta aplicação foi desenvolvida com Angular Material.',
+    cols: 2,
+    rows: 2,
+    color: '#3f51b5',
+    imageUrl: 'assets/images/angular_material_logo.png'
+  }, {
+    text: 'A navegação entre os componentes desta aplicação se vale do mecanismo de Routing do Angular.',
+    cols: 2,
+    rows: 2,
+    color: '#fafafa',
+    imageUrl: 'assets/images/angular_routing_logo.png'
+  }, {
+    text: 'Os eventos estão sendo tratados com RxJs, o que permite que a aplicação seja mais reativa à interação com o usuário.',
+    cols: 2,
+    rows: 2,
+    color: '#333',
+    imageUrl: 'assets/images/rxjs_logo.png'
+  }];
 
   constructor() { }
 
